d032aec267ed358e0130c67b302977cdf0d063e6
Alex Auyang <aa17717@ic.ac.uk>
2019-08-28 17:15:43 +0200

    done

--- db/seeds/g_recipes_seed.rb
+++ db/seeds/g_recipes_seed.rb
@@ -3,11 +3,11 @@ pomegranate_sorbet = Recipe.new(
   name: "Pomegranate Sorbet",
   hours_preparation: 4,
   description: "Pomegranates have a sweet flavour with a hint of sharpness and a wonderful colour. This sorbet makes the most of these qualities and is simple to make.",
-  instructions: "1. Cut the pomegranates in half and reserve a few of the seeds for decoration. Squeeze as you would a lemon or an orange, to produce about 500ml juice. Sieve to remove any pips or pith.\n
-  2. Stir the sugar, lemon juice and grenadine into the pomegranate juice and stir until the sugar has dissolved. Pour the mixture into a plastic freezer container, preferably square or rectangular, with a flat base - a 1-litre capacity clean and dry ice cream container is ideal. Then cover and place in the coldest part of the freezer.\n
-  3. After about 1-1½ hours, the mixture should have frozen around the edges. Use an electric beater or balloon whist to beat the sorbet until it becomes a uniform slush. Quickly return to the freezer.\n
+  instructions: "1. Cut the pomegranates in half and reserve a few of the seeds for decoration. Squeeze as you would a lemon or an orange, to produce about 500ml juice. Sieve to remove any pips or pith.,;;;
+  2. Stir the sugar, lemon juice and grenadine into the pomegranate juice and stir until the sugar has dissolved. Pour the mixture into a plastic freezer container, preferably square or rectangular, with a flat base - a 1-litre capacity clean and dry ice cream container is ideal. Then cover and place in the coldest part of the freezer.,;;;
+  3. After about 1-1½ hours, the mixture should have frozen around the edges. Use an electric beater or balloon whist to beat the sorbet until it becomes a uniform slush. Quickly return to the freezer.,;;;
   4. Repeat this process twice, leaving the mixture to partially freeze between each beating. After the final stage, the sorbet will need at least 45 minutes of further freezing before serving. To serve, decorate with reserved pomegranate seeds.",
-  shopping_list: "5-6 pomegranates\n150g caster sugar\n3 tbsp lemon juice\n1 tbsp Monin Sirop de Grenadine\nHandful of blueberries",
+  shopping_list: "5-6 pomegranates,;;;150g caster sugar,;;;3 tbsp lemon juice,;;;1 tbsp Monin Sirop de Grenadine,;;;Handful of blueberries",
   serves: 6
   )
 
@@ -19,10 +19,10 @@ pomegranate_beef = Recipe.new(
   name: "Pomegranate Beef Short Ribs",
   hours_preparation: 3.75,
   description: "Pomegranate juice and red wine compose a rich ruby-red sauce for tender beef short ribs. Garnish with jewel-like pomegranate seeds just before serving—they give tang and color and make the dish look fancy in a flash.",
-  instructions: "1. Preheat oven to 275 degrees. In a large heavy pot with a tight-fitting lid, heat 2 tablespoons oil over high. Season short ribs with salt and pepper. In batches, brown ribs on all sides. Transfer to a plate, pour off oil, and wipe loose bits out of pot.\n
-  2. Reduce heat to medium-high and add remaining 3 tablespoons oil to pot. Add onion, garlic, and thyme. Cook, scraping up browned bits with a wooden spoon, until onion is softened, 5 minutes. Add flour and stir to coat. Whisk in pomegranate juice and wine and bring to a boil, stirring frequently. Return ribs to pot, cover, and transfer to oven.\n
+  instructions: "1. Preheat oven to 275 degrees. In a large heavy pot with a tight-fitting lid, heat 2 tablespoons oil over high. Season short ribs with salt and pepper. In batches, brown ribs on all sides. Transfer to a plate, pour off oil, and wipe loose bits out of pot.,;;;
+  2. Reduce heat to medium-high and add remaining 3 tablespoons oil to pot. Add onion, garlic, and thyme. Cook, scraping up browned bits with a wooden spoon, until onion is softened, 5 minutes. Add flour and stir to coat. Whisk in pomegranate juice and wine and bring to a boil, stirring frequently. Return ribs to pot, cover, and transfer to oven.,;;;
   3. Bake until ribs are easily pierced with the tip of a paring knife, about 3 hours. With a slotted spoon, carefully transfer ribs to a large platter. Strain liquid into a fat separator, let sit briefly, and return to pot, discarding fat (or, skim off fat with a spoon). Bring to a boil over medium-high and cook until reduced to a sauce-like consistency, 10 minutes. Strain through a fine-mesh sieve, season with salt and pepper, and pour over ribs. Top with pomegranate seeds.",
-  shopping_list: "1/2 cup pomegranate seeds, for serving\n3 cups pomegranate juice\n4 pounds bone-in beef short ribs, cut into 3-inch pieces\n3 cloves garlic, smashed and peeled\n1 large yellow onion, cut into wedges\n10 sprigs thyme\n1 cup dry red wine, such as Merlot.",
+  shopping_list: "1/2 cup pomegranate seeds, for serving,;;;3 cups pomegranate juice,;;;4 pounds bone-in beef short ribs, cut into 3-inch pieces,;;;3 cloves garlic, smashed and peeled,;;;1 large yellow onion, cut into wedges,;;;10 sprigs thyme,;;;1 cup dry red wine, such as Merlot.",
   serves: 8
   )
 
@@ -34,10 +34,10 @@ pomegranate_pizza = Recipe.new(
   name: "Pomegranate, Eggplant and Pistachio Pizza",
   hours_preparation: 0.5,
   description: "Pomegranate as a pizza topping? You bet! The colorful, crunchy seeds make a lovely foil for eggplant, pistachios, , and cilantro.",
-  instructions: "1. Preheat oven to 475 degrees. Form dough (as directed in Basic Pizza Dough recipe) in a 9-by-13-inch rimmed baking sheet. Arrange eggplant lengthwise on dough, 3 slices side-by-side on top half, 3 on bottom half; press into dough. Drizzle with oil, and sprinkle with salt and red-pepper flakes.\n
-  2. Bake until cooked through and crust is golden on edges and bottom, about 18 minutes.\n
+  instructions: "1. Preheat oven to 475 degrees. Form dough (as directed in Basic Pizza Dough recipe) in a 9-by-13-inch rimmed baking sheet. Arrange eggplant lengthwise on dough, 3 slices side-by-side on top half, 3 on bottom half; press into dough. Drizzle with oil, and sprinkle with salt and red-pepper flakes.,;;;
+  2. Bake until cooked through and crust is golden on edges and bottom, about 18 minutes.,;;;
   3. Using a metal spatula, slide pie onto a cutting board. Sprinkle with pistachios, feta, pomegranate seeds, and cilantro. Slice into 6 pieces and serve.",
-  shopping_list: "1/3 cup pomegranate seeds (from 1 pomegranate)\n1/3 recipe Basic Pizza Dough\n1 to 2 small eggplants\nExtra-virgin olive oil\nRed-pepper flakes\n1/2 cup salted pistachios, chopped\n1/3 cup crumbled feta\n1/4 cup lightly packed cilantro leaves.",
+  shopping_list: "1/3 cup pomegranate seeds (from 1 pomegranate),;;;1/3 recipe Basic Pizza Dough,;;;1 to 2 small eggplants,;;;Extra-virgin olive oil,;;;Red-pepper flakes,;;;1/2 cup salted pistachios, chopped,;;;1/3 cup crumbled feta,;;;1/4 cup lightly packed cilantro leaves.",
   serves: 8
   )
 
@@ -50,7 +50,7 @@ pomegranate_sangria = Recipe.new(
   hours_preparation: 0.2,
   description: "Gussy up white or rose wine with pomegranate, apples, and for a party-perfect (and Rosh Hashanah-friendly!) autumn sangria.",
   instructions: "1.Bring 1/4 cup apple juice to a simmer in a small saucepan. Stir in honey until dissolved. Fill a large pitcher halfway with ice, then pour the juice mixture over it. Add wine, 1 cup apple juice, pomegranate juice, pomegranate seeds, and apple slices. Stir to combine. Serve in glasses garnished with more pomegranate seeds and apple slices.",
-  shopping_list: "1/2 cup pomegranate seeds, plus more for garnish\n 1 cup pomegranate juice\n2 Lady apples, thinly sliced into rounds, plus more for garnish\n1/4 cup honey\n1 1/4 cup apple juice, divided.",
+  shopping_list: "1/2 cup pomegranate seeds, plus more for garnish,;;; 1 cup pomegranate juice,;;;2 Lady apples, thinly sliced into rounds, plus more for garnish,;;;1/4 cup honey,;;;1 & 1/4 cup apple juice, divided.",
   serves: 4
   )
 
@@ -62,9 +62,9 @@ pomegranate_potato = Recipe.new(
   name: "Sweet Potatoes with Pomegranate, Coconut, and Lime",
   hours_preparation: 4,
   description: "This dish adds tropical flavor to a nutrient-rich sweet potato by topping it with cilantro, coconut flakes, and pomegranate seeds.",
-  instructions: "1. Heat oven to 400 degrees. Arrange sweet potatoes, pricked with a fork, on rimmed baking sheet and roast until tender, about 45 minutes. Let cool slightly.\n
+  instructions: "1. Heat oven to 400 degrees. Arrange sweet potatoes, pricked with a fork, on rimmed baking sheet and roast until tender, about 45 minutes. Let cool slightly.,;;;
   2. Slice tops and mash sweet potatoes with a fork. Divide coconut milk, coconut flakes, cilantro, and pomegranate seeds among sweet potatoes. Season with salt. Garnish with additional sprigs of cilantro and serve with lime wedges.",
-  shopping_list: "1 cup pomegranate seeds\n1/4 toasted unsweetened coconut flakes\n2 tablespoons chopped cilantro\n1/2 cup light coconut milk\n4 sweet potatoes.",
+  shopping_list: "1 cup pomegranate seeds,;;;1/4 toasted unsweetened coconut flakes,;;;2 tablespoons chopped cilantro,;;;1/2 cup light coconut milk,;;;4 sweet potatoes.",
   serves: 4
   )
 
@@ -77,7 +77,7 @@ pomegranate_relish = Recipe.new(
   hours_preparation: 0.333333333,
   description: "Pair this sweet and tangy relish with our Roast Turkey with Rosemary and Lemon. Use any leftovers as a sandwich spread or stirred into yogurt as a dip.",
   instructions: "1. In a medium saucepan, heat oil over medium-high. Add shallot; cook, stirring constantly, until softened, 3 minutes. Add cranberries, pomegranate juice, and brown sugar; season with salt and pepper. Bring to a boil, then reduce to a simmer and cook, stirring occasionally, until cranberries have burst and sauce is slightly thickened, 10 minutes. Remove from heat. Stir in pomegranate seeds (if using). Let cool to room temperature before serving.",
-  shopping_list: "1 cup pomegranate juice\n1/2 cup pomegranate seeds (optional)\n1 bag (12 ounces) fresh or frozen cranberries\n1/2 cup packed dark-brown sugar\n1 large shallot, finely chopped.",
+  shopping_list: "1 cup pomegranate juice,;;;1/2 cup pomegranate seeds (optional),;;;1 bag (12 ounces) fresh or frozen cranberries,;;;1/2 cup packed dark-brown sugar,;;;1 large shallot, finely chopped.",
   serves: 10
   )
 
